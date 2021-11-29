fab422c8ce6663de92b9e655efab89079dda9c4f
Jeffery <parkjon8@gmail.com>
2021-11-29 19:32:12 +0000

    Refactored tail.js to export function

--- tail.js
+++ tail.js
@@ -17,3 +17,5 @@ const result = tail(["Hello", "Lighthouse", "Labs"]);
 console.log(assertEqual(result.length, 2)); // ensure we get back two elements
 console.log(assertEqual(result[0], "Lighthouse")); // ensure first element is "Lighthouse"
 console.log(assertEqual(result[1], "Labs")); // ensure second element is "Labs"
+
+fdfddfd
