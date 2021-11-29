dc60e998832a923e6dfd9cd45998b671a8ec8b6f
Jeffery Park <85148711+smjeffery22@users.noreply.github.com>
2021-11-29 10:10:24 -0500

    Add files via upload
    Added test files for head and assertEqual

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require("../assertEqual");
+
+// TEST CODE
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual("Lighthouse Labs", "Lighthouse Labs"));
+console.log(assertEqual(1, 1));
+console.log(assertEqual(1, 2));
