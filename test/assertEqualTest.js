f036f5d5672a8ad910bb50561dbb21213baa417b
Jeffery Park <85148711+smjeffery22@users.noreply.github.com>
2021-11-29 10:14:39 -0500

    Delete test directory
    Delete test folder

--- test/assertEqualTest.js
+++
@@ -1,7 +0,0 @@
-const assertEqual = require("../assertEqual");
-
-// TEST CODE
-console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
-console.log(assertEqual("Lighthouse Labs", "Lighthouse Labs"));
-console.log(assertEqual(1, 1));
-console.log(assertEqual(1, 2));
