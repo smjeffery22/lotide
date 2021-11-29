fa7473c03d49eea3a4959f212a28caad81bb902e
Jeffery <parkjon8@gmail.com>
2021-11-29 14:17:08 +0000

    Refactored head.js and assertEqual.js to export the functions

--- assertEqual.js
+++ assertEqual.js
@@ -3,8 +3,4 @@ const assertEqual = function(actual, expected) {
   return actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
 };
 
-// TEST CODE
-console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
-console.log(assertEqual("Lighthouse Labs", "Lighthouse Labs"));
-console.log(assertEqual(1, 1));
-console.log(assertEqual(1, 2));
+module.exports = assertEqual;
